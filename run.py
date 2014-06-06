3a7e974ad09a729d4c6b08ae84a11c03da702caf
EricsonWillians <ericsonwrp@gmail.com>
2014-06-05 23:09:21 -0300

    x

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 ====================================================================
 
-PyclassicS 1.2 - Snake Game
+PyclassicS 1.2 - Snake Game %
 Copyright (C) <2014>  <Ericson Willians.>
 
 This program is free software: you can redistribute it and/or modify
